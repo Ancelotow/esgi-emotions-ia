0590792609b9152aaa806393106c07f3039de9f4
Owen ANCELOT <owen.ancelot@la-projets.com>
2024-01-27 16:06:07 +0100

    App eye detection ✨

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 
 faceCascade = cv2.CascadeClassifier('lib/haarcascade.xml')
+eyeCascade = cv2.CascadeClassifier('lib/eye_cascade.xml')
 cap = cv2.VideoCapture(0)
 cap.set(3,640) # set Width
 cap.set(4,480) # set Height
@@ -16,10 +17,23 @@ while (True):
         minNeighbors=5,
         minSize=(20, 20)
     )
+    eyes = eyeCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.2,
+        minNeighbors=5,
+        minSize=(20, 20)
+    )
+
     for (x, y, w, h) in faces:
         cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
         roi_gray = gray[y:y + h, x:x + w]
         roi_color = img[y:y + h, x:x + w]
+
+    for (x, y, w, h) in eyes:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        roi_gray = gray[y:y + h, x:x + w]
+        roi_color = img[y:y + h, x:x + w]
+
     cv2.imshow('video', img)
     k = cv2.waitKey(30) & 0xff
     if k == 27:  # press 'ESC' to quit
