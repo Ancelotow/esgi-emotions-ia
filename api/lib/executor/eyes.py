98a66ad5733952012cec764222f25429c781b08d
Owen ANCELOT <owen.ancelot@la-projets.com>
2024-02-09 19:34:26 +0100

    Fix an improve learning for eye ✨

--- api/lib/executor/eyes.py
+++ api/lib/executor/eyes.py
@@ -1,14 +1,13 @@
 from matplotlib import pyplot as plt
+from skimage.transform import resize
 from sklearn.metrics import confusion_matrix, accuracy_score, ConfusionMatrixDisplay
 from sklearn.neural_network import MLPClassifier
 import os
 from os.path import exists
-from PIL import Image
-import platform
-import cv2
+from skimage import io
 
 from api.lib.dataset import write_header_dataset, write_dataset_file, rescale_image
-from api.lib.machine_learning import get_data, load_model, save_model
+from api.lib.machine_learning import load_model, save_model
 
 PATH = "eyes"
 # DATASETS
@@ -22,11 +21,27 @@ DATASET_NAME_TRAIN = "../../dataset/" + PATH + "/dataset_train.csv"
 DATASET_NAME_PREDICT = "../../dataset/" + PATH + "/dataset_predict.csv"
 
 # CLASSIFICATION
-CLASSIFICATION = ["amber", "blue", "brown", "gray", "grayscale", "green", "hazel", "red"]
+CLASSIFICATION = ["amber", "blue", "brown", "gray", "green", "hazel", "red"]
 
 # LEARNING
 DO_LEARN = True
-HIDDEN_LAYER_SIZE = (150, 250, 350, 250, 150)
+HIDDEN_LAYER_SIZE = (150, 250, 300, 250, 150)
+
+
+def get_data(dir):
+    inputs = []
+    outputs = []
+    for i in range(len(CLASSIFICATION)):
+        classification_folder = CLASSIFICATION[i]
+        path = dir + "/" + classification_folder
+        filenames = os.listdir(path)
+        for fn in filenames:
+            img = io.imread(path + "/" + fn)
+            img_resized = resize(img, (64, 64))
+            inputs.append(img_resized.flatten().tolist())
+            outputs.append(i)
+    return inputs, outputs
+
 
 if __name__ == '__main__':
     # os.remove(DATASET_FILE)
@@ -36,7 +51,7 @@ if __name__ == '__main__':
     # write_dataset_file(DATASET_TEST, CLASSIFICATION, DATASET_FILE)
 
     print(os.getcwd())
-    inputs, outputs = get_data("TEST", PATH)
+    inputs, outputs = get_data(DATASET_TRAIN)
     if not exists(FILE_MODEL) or DO_LEARN:
         # TRAIN
         classifier = MLPClassifier(hidden_layer_sizes=HIDDEN_LAYER_SIZE, max_iter=100)
