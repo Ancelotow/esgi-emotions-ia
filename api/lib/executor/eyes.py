67d6eda1b265eddb7428ce7d618815f19d2b5c9e
Owen ANCELOT <owen.ancelot@la-projets.com>
2024-02-11 16:20:36 +0100

    Eye MLClassifier ✨

--- api/lib/executor/eyes.py
+++ api/lib/executor/eyes.py
@@ -1,65 +1,99 @@
+import cv2
 from matplotlib import pyplot as plt
-from skimage.transform import resize
 from sklearn.metrics import confusion_matrix, accuracy_score, ConfusionMatrixDisplay
 from sklearn.neural_network import MLPClassifier
 import os
 from os.path import exists
 from skimage import io
+from sklearn.model_selection import train_test_split
 from api.lib.machine_learning import load_model, save_model
+from api.lib.preprocessing import preprocess_for_eyes
 
 PATH = "eyes"
-# DATASETS
 FILE_MODEL = "../../dataset/" + PATH + "/model.dat"
-DATASET_TRAIN = "../../dataset/" + PATH + "/train"
-DATASET_TEST = "../../dataset/" + PATH + "/test"
-DATASET_FILE = "../../dataset/" + PATH + "/dataset_test.csv"
-
-DATASET_NAME_TEST = "../../dataset/" + PATH + "/dataset_train.csv"
-DATASET_NAME_TRAIN = "../../dataset/" + PATH + "/dataset_train.csv"
-DATASET_NAME_PREDICT = "../../dataset/" + PATH + "/dataset_predict.csv"
+DATASET = "../../dataset/" + PATH + "/train"
+TEMP_DIR = "temp"
 
 # CLASSIFICATION
-CLASSIFICATION = ["amber", "blue", "brown", "gray", "green", "hazel", "red"]
+EYE_CLASS = ["amber", "blue", "brown", "gray", "green", "hazel", "red"]
 
 # Parameters
-DO_LEARN = False
+DO_LEARN = True
 HIDDEN_LAYER_SIZE = (150, 250, 300, 250, 150)
 MAX_ITER = 200
-CURRENT_DATASET = DATASET_TEST
 
 
-def get_data(dir):
+def transform(directory):
+    for i in range(len(EYE_CLASS)):
+        classification_folder = EYE_CLASS[i]
+        path = directory + "/" + classification_folder
+        path_temp = path + "/" + TEMP_DIR
+        filenames = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
+
+        if not os.path.exists(path_temp):
+            os.makedirs(path_temp)
+
+        for fn in filenames:
+            img_path = path + "/" + fn
+            img_path_tmp = path_temp + "/" + fn
+            img = io.imread(img_path)
+            img_transformed = preprocess_for_eyes(img)
+            cv2.imwrite(img_path_tmp, img_transformed)
+
+
+def get_data(directory):
     inputs = []
     outputs = []
-    for i in range(len(CLASSIFICATION)):
-        classification_folder = CLASSIFICATION[i]
-        path = dir + "/" + classification_folder
+    for i in range(len(EYE_CLASS)):
+        classification_folder = EYE_CLASS[i]
+        path = directory + "/" + classification_folder + "/" + TEMP_DIR
         filenames = os.listdir(path)
         for fn in filenames:
             img = io.imread(path + "/" + fn)
-            img_resized = resize(img, (64, 64))
-            inputs.append(img_resized.flatten().tolist())
+            img_bgr = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+            inputs.append(img_bgr.flatten().tolist())
             outputs.append(i)
     return inputs, outputs
 
 
+def prediction(model, inputs, outputs):
+    predict = model.predict(inputs)
+    margin_errors = confusion_matrix(outputs, predict, normalize='true')
+    score = accuracy_score(outputs, predict)
+    return score, margin_errors
+
+
 if __name__ == '__main__':
+    print("Transforming...")
+    transform(DATASET)
     print("Getting data...")
-    inputs, outputs = get_data(CURRENT_DATASET)
+    all_inputs, all_outputs = get_data(DATASET)
+    train_inputs, test_inputs, train_outputs, test_outputs = train_test_split(all_inputs, all_outputs, test_size=0.6)
     if not exists(FILE_MODEL) or DO_LEARN:
-        # TRAIN
         print("Training...")
         classifier = MLPClassifier(hidden_layer_sizes=HIDDEN_LAYER_SIZE, max_iter=MAX_ITER)
-        classifier.fit(inputs, outputs)
+        classifier.fit(train_inputs, train_outputs)
         save_model(classifier, FILE_MODEL)
     else:
         classifier = load_model(FILE_MODEL)
 
     print("Predicting...")
-    predict = classifier.predict(inputs)
-    margin_errors = confusion_matrix(outputs, predict, normalize='true')
-    score = accuracy_score(outputs, predict)
-    disp = ConfusionMatrixDisplay(confusion_matrix=margin_errors, display_labels=CLASSIFICATION)
-    disp.plot()
+    train_score, train_errors = prediction(classifier, train_inputs, train_outputs)
+    test_score, test_errors = prediction(classifier, test_inputs, test_outputs)
+
+    plt.figure(figsize=(10, 10))
+    train_disp = ConfusionMatrixDisplay(confusion_matrix=train_errors, display_labels=EYE_CLASS)
+    train_disp.plot()
+    plt.title('Train Confusion Matrix')
     plt.show()
-    print("Score : " + str(score) + "\n")
+
+    plt.figure(figsize=(10, 10))
+    test_disp = ConfusionMatrixDisplay(confusion_matrix=test_errors, display_labels=EYE_CLASS)
+    test_disp.plot()
+    plt.title('Test Confusion Matrix')
+    plt.show()
+
+    train_score_formatted = "{:.2f}".format(train_score * 100)
+    test_score_formatted = "{:.2f}".format(test_score * 100)
+    print(f"\nScore training: {str(train_score_formatted)}%")
+    print(f"Score validation: {str(test_score_formatted)}%")
--- api/lib/preprocessing.py
+++ api/lib/preprocessing.py
@@ -2,19 +2,26 @@ import cv2
 import numpy as np
 from skimage.transform import resize
 
+lower_bound = [90, 50, 50]
+upper_bound = [130, 255, 255]
 
-def transform_for_feeling(img):
+
+def preprocess_for_feeling(img):
     img_resized = resize(img, (64, 64))
-
-    # Increase contrast
     img_resized = cv2.equalizeHist((img_resized * 255).astype(np.uint8))
-
-    # Apply Sobel filter
     sobelx = cv2.Sobel(np.float32(img_resized), cv2.CV_64F, 1, 0, ksize=9)
     sobely = cv2.Sobel(np.float32(img_resized), cv2.CV_64F, 0, 1, ksize=9)
     sobel = np.hypot(sobelx, sobely)
     max_value = np.max(sobel)
     if max_value == 0:
-        max_value = 1e-5  # small constant
+        max_value = 1e-5
     sobel *= 255.0 / max_value
     return sobel
+
+
+def preprocess_for_eyes(img):
+    bgr_img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+    #hsv_img = cv2.cvtColor(bgr_img, cv2.COLOR_BGR2HSV)
+    img_resized = resize(bgr_img, (30, 30))
+    img_resized = (img_resized * 255).astype(np.uint8)
+    return img_resized
