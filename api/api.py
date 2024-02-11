920768fc98e313ba03191a69d1024ad2debb29e9
Owen ANCELOT <owen.ancelot@la-projets.com>
2024-02-11 20:35:21 +0100

    Eye detection in app

--- api/api.py
+++ api/api.py
@@ -2,9 +2,11 @@ import pickle
 
 import cv2
 import numpy as np
+
+from api.lib.executor.eyes import EYE_CLASS
 from api.lib.executor.feelings import FEELINGS_CLASS
 from tensorflow.keras.models import load_model
-from api.lib.preprocessing import preprocess_for_feeling, preprocess_for_age
+from api.lib.preprocessing import preprocess_for_feeling, preprocess_for_age, preprocess_for_eyes
 
 
 def load_dat_model(file_model):
@@ -15,6 +17,7 @@ def load_dat_model(file_model):
 
 feelings_model = load_model("../api/dataset/feelings/model.h5")
 age_model = load_dat_model("../api/dataset/age/model.dat")
+eye_model = load_dat_model("../api/dataset/eyes/model.dat")
 
 
 def feeling_detection(image):
@@ -33,3 +36,13 @@ def age_detection(image):
     predict = age_model.predict([intput])
     predict_age = predict[0]
     return int(predict_age)
+
+
+def eye_detection(image):
+    img_clr = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    img_transformed = preprocess_for_eyes(img_clr)
+    intput = img_transformed.flatten().tolist()
+    cv2.imwrite("eyes.png", img_transformed)
+    predict = eye_model.predict([intput])
+    predict_eye = EYE_CLASS[int(predict[0])]
+    return predict_eye
--- app/main.py
+++ app/main.py
@@ -1,22 +1,15 @@
 import tkinter as tk
 from PIL import Image, ImageTk
 import cv2
-from api.api import feeling_detection, age_detection
+from api.api import feeling_detection, age_detection, eye_detection
 
 faceCascade = cv2.CascadeClassifier('lib/haarcascade.xml')
 eyeCascade = cv2.CascadeClassifier('lib/eye_cascade.xml')
 cap = cv2.VideoCapture(0)
-text_id = None
 
-
-def feeling_detector(x, y, w, h, gray):
-    roi_gray = gray[y:y + h, x:x + w]
-    return feeling_detection(roi_gray)
-
-
-def age_detector(x, y, w, h, gray):
-    roi_gray = gray[y:y + h, x:x + w]
-    return age_detection(roi_gray)
+def eye_detector(x, y, w, h, img):
+    roi_clr = img[y:y + h, x:x + w]
+    return eye_detection(roi_clr)
 
 
 def face_detector(img, gray):
@@ -28,22 +21,26 @@ def face_detector(img, gray):
     )
     for (x, y, w, h) in faces:
         cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-        feeling = feeling_detector(x, y, w, h, gray)
-        age = age_detector(x, y, w, h, gray)
+        feeling = feeling_detection(gray[y:y + h, x:x + w])
+        age = age_detection(gray[y:y + h, x:x + w])
+        colors = eyes_detector(img, gray)
+        color_txt = ""
+        if len(colors) > 0:
+            color_txt = colors[0]
 
-        # Create a semi-transparent rectangle as a background for the text
+        # Fond semi-transparent
         overlay = img.copy()
-        cv2.rectangle(overlay, (x, y - 80), (x + w, y), (0, 0, 0), -1)
-
-        # Apply the overlay
+        cv2.rectangle(overlay, (x, y - 100), (x + w, y), (0, 0, 0), -1)
         cv2.addWeighted(overlay, 0.5, img, 0.5, 0, img)
 
-        # Put the text on the image
+        # Descriptions
+        cv2.putText(img, f"Age : {age}", (x, y - 70), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 255, 255), 2)
+        cv2.putText(img, f"Yeux : {color_txt}", (x, y - 40), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 255, 255), 2)
         cv2.putText(img, f"Emotion : {feeling}", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 255, 255), 2)
-        cv2.putText(img, f"Age : {age}", (x, y - 40), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 255, 255), 2)
 
 
 def eyes_detector(img, gray):
+    colors = []
     eyes = eyeCascade.detectMultiScale(
         gray,
         scaleFactor=1.2,
@@ -52,8 +49,8 @@ def eyes_detector(img, gray):
     )
     for (x, y, w, h) in eyes:
         #cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        roi_gray = gray[y:y + h, x:x + w]
-        roi_color = img[y:y + h, x:x + w]
+        colors.append(eye_detection(img[y:y + h, x:x + w]))
+    return colors
 
 
 def update_frame():
@@ -65,7 +62,7 @@ def update_frame():
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
         face_detector(img, gray)
-        eyes_detector(img, gray)
+        #eyes_detector(img, gray)
 
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         image = Image.fromarray(img)
